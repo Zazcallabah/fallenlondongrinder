62465747b99f45c6ba101997f87485b8b537637e
Peter Hamberg <zazcallabah@gmail.com>
2019-12-09 15:30:39 +0100

    cleanup entities, sort aplhabetically

--- fl/Entities.cs
+++ fl/Entities.cs
@@ -32,6 +32,7 @@ namespace fl
 	// NOTE maybe you actually bought the stuff you needed for this requirement though? how to distinguish this??
 
 	//
+
 	public enum HasActionsLeft
 	{
 		Available,
@@ -40,19 +41,6 @@ namespace fl
 		Mismatch,
 	}
 
-	public class ActionList
-	{
-		public string[] automaton;
-		public string[] main;
-	}
-
-	public class CardAction
-	{
-		public string name;
-		public string[] require;
-		public string action;
-		public long? eventId;
-	}
 	public class Acquisition
 	{
 		public string Key;
@@ -63,199 +51,18 @@ namespace fl
 		public int? Reward;
 		public CardAction[] Cards;
 	}
-
-	public class ForcedAction
+	public class ActionList
 	{
-		public string[] Conditions;
-		public string Action;
+		public string[] automaton;
+		public string[] main;
 	}
-
-	public class SuccessMessage
+	public class BasicMessage
 	{
-		public bool? isSuccess;
-		public string message;
-	}
-
-	public class PlanQuery
-	{
-		public string name;
-		public string planKey;
-		public int? id;
-	}
-
-	public class LockedAreaData
-	{
-		public string name;
-		public string[] require;
-		public bool? forced;
-	}
-
-	public class MapEntry
-	{
-		public string name;
-		public string description;
-		public bool showOps;
-		public bool hideName;
-		public bool premiumSubRequired;
-		public int id;
-	}
-
-	public class Map
-	{
-		public MapEntry[] areas;
-	}
-
-
-	public class UserSetting
-	{
-		public string name;
-		public bool canTravel;
-
-		public int id;
-
-		public bool itemsUsableHere;
-	}
-
-	public class UserArea
-	{
-		public int id;
-		public string name;
-		public string description;
-		public string image;
-		public bool hideName;
-		public bool showOps;
-		public bool premiumSubRequired;
-	}
-
-	public class User
-	{
-		public UserArea area;
-		public UserSetting setting;
-		public bool hasCharacter;
-		public string privilegeLevel;
-		public UserData user;
-
-	}
-
-	public class ShopItem
-	{
-		public ShopAvailability availability;
-		public bool forSale;
-	}
-
-	public class ShopAvailability
-	{
-		public ShopItemQuality quality;
-		public int cost;
-		public int sellPrice;
-		public long id;
-
-	}
-
-	public class ShopItemQuality
-	{
-		public long id;
-		public string name;
-		public string description;
-		public long useEventId;
-		public string category;
-		public string image;
-	}
-
-	public class SuccessResult
-	{
-		public bool isSuccess;
-	}
-
-	public class Plans
-	{
-		public Plan[] active;
-		public Plan[] complete;
-	}
-
-	public class Plan
-	{
-		public PlanBranch branch;
-		public string areaName;
-		public long id;
-	}
-
-	public class PlanResult
-	{
-		public Plan plan;
-		public string message;
-		public bool isSuccess;
-
-	}
-
-	public class PlanBranch
-	{
-		public QualityReq[] qualityRequirements;
-		public int id;
-		public string planKey;
-		public string name;
-		public string description;
-		public int currencyCost;
-		public int actionCost;
-		public string buttonText;
-		public string image;
-		public bool isLocked;
-		public int ordering;
-		public bool actionLocked;
-		public bool currencyLocked;
-		public bool qualityLocked;
-		public Challenge[] challenges;
-	}
-
-	public class Challenge
-	{
-		public string name;
-		public long id;
-		public int targetNumber;
-		public string description;
-		public string category;
-		public string nature;
 		public string type;
-		public bool canAffordSecondChance;
+		public string message;
 		public string image;
-		public int secondChanceId;
-		public int secondChanceLevel;
-
-	}
-	public class QualityReq
-	{
-		public long qualityId;
-		public long id;
-		public string qualityName;
 		public string tooltip;
-		public string category;
-		public string nature;
-		public string status;
-		public bool isCost;
-		public string image;
-		public string availableAtMessage;
 	}
-	public class Storylet
-	{
-		public string name;
-		public string teaser;
-		public string category;
-		public QualityReq[] qualityRequirements;
-		public string image;
-		public long id;
-
-		public bool? isLocked;
-
-		public string frequency;
-		public string distribution;
-		public string description;
-		public string urgency;
-		public bool isInEventUseTree;
-		public bool? canGoBack;
-
-		public Branch[] childBranches;
-	}
-
 	public class Branch
 	{
 		public string name;
@@ -274,39 +81,38 @@ namespace fl
 		public string image;
 		public long id;
 	}
-
-
-	public class StoryletList
-	{
-		public int actions;
-		public string phase;
-		public Storylet[] storylets;
-		public Storylet storylet;
-		public EndStorylet endStorylet;
-		public SocialAct socialAct;
-		public Messages messages;
-		public bool isSuccess;
-	}
-
-	public class Myself
-	{
-		public Character character;
-		public PossessionCategory[] possessions;
-	}
-	public class TransactionResult
-	{
-		public Possession[] possessionsChanged;
-		public string message;
-		public bool isSuccess;
-	}
-
-	public class Domicile
+	public class Card
 	{
 		public string name;
-		public string description;
-		public int maxHandSize;
+		public long eventId;
+		public string category;
+		public string unlockedWithDescription;
+		public string teaser;
+		public bool isAutofire;
+		public string stickiness;
+		public QualityReq[] qualityRequirements;
 	}
-
+	public class CardAction
+	{
+		public string name;
+		public string[] require;
+		public string action;
+		public long? eventId;
+	}
+	public class Challenge
+	{
+		public string name;
+		public long id;
+		public int targetNumber;
+		public string description;
+		public string category;
+		public string nature;
+		public string type;
+		public bool canAffordSecondChance;
+		public string image;
+		public int secondChanceId;
+		public int secondChanceLevel;
+	}
 	public class Character
 	{
 		public string name;
@@ -319,41 +125,112 @@ namespace fl
 		public UserData user;
 		public int id;
 	}
-	public class OutfitSlot
+	public class Domicile
 	{
 		public string name;
-		public long? qualityId;
+		public string description;
+		public int maxHandSize;
 	}
-
-	public class Outfit
+	public class EndStorylet
+	{
+		public int rootEventId;
+		public int currentActionsRemaining;
+		[JsonProperty(PropertyName = "event")]
+		public Event eventValue;
+		public bool isLinkingEvent;
+		public bool isDirectLinkingEvent;
+		public int maxActionsAllowed;
+		public bool premiumBenefitsApply;
+		public string image;
+		public bool canGoAgain;
+	}
+	public class Event
 	{
 		public string name;
-		public bool selected;
+		public string description;
+		public bool isInEventUseTree;
+		public string frequency;
+		public string image;
 		public long id;
 	}
-
-	public class UserData
+	public class Exchange
 	{
 		public string name;
-		public string emailAddress;
-		public int nex;
+		public string title;
+		public string description;
+		public Shop[] shops;
+		public string image;
 		public int id;
 	}
-
+	public class ExchangeResponse
+	{
+		public Exchange exchange;
+		public bool isSuccess;
+	}
+	public class ForcedAction
+	{
+		public string[] Conditions;
+		public string Action;
+	}
+	public class Friend
+	{
+		public long userId;
+		public long id;
+		public string name;
+		public string userName;
+	}
+	public class Interaction
+	{
+		public string type;
+		public string image;
+		public long relatedId;
+		public string description;
+		public string date;
+		public string ago;
+	}
+	public class InviteeData
+	{
+		public long branchId;
+		public string actQReqText;
+		public string actInviterQReqText;
+		public int addedFriendId;
+		public Friend[] eligibleFriends;
+	}
+	public class LockedAreaData
+	{
+		public string name;
+		public string[] require;
+		public bool? forced;
+	}
+	public class Map
+	{
+		public MapEntry[] areas;
+	}
+	public class MapEntry
+	{
+		public string name;
+		public string description;
+		public bool showOps;
+		public bool hideName;
+		public bool premiumSubRequired;
+		public int id;
+	}
+	public class Messages
+	{
+		public BasicMessage[] difficultyMessages;
+		public BasicMessage[] defaultMessages;
+		public StandardMessage[] standardMessages;
+	}
 	public class Move
 	{
 		public UserArea area;
 		public bool isSuccess;
 	}
-
-	public class PossessionCategory
+	public class Myself
 	{
-		public string[] categories;
-		public string name;
-		public Possession[] possessions;
-		public string appearance;
+		public Character character;
+		public PossessionCategory[] possessions;
 	}
-
 	public class Opportunity
 	{
 		public Card[] displayCards;
@@ -361,29 +238,136 @@ namespace fl
 		public int eligibleForCardsCount;
 		public int maxHandSize;
 		public int maxDeckSize;
-
 	}
-
-	public class Card
+	public class Outfit
 	{
 		public string name;
-		public long eventId;
-		public string category;
-		public string unlockedWithDescription;
-		public string teaser;
-		public bool isAutofire;
-		public string stickiness;
-		public QualityReq[] qualityRequirements;
+		public bool selected;
+		public long id;
 	}
-
-	public class BasicMessage
+	public class OutfitSlot
 	{
-		public string type;
-		public string message;
-		public string image;
-		public string tooltip;
+		public string name;
+		public long? qualityId;
 	}
-
+	public class Plan
+	{
+		public PlanBranch branch;
+		public string areaName;
+		public long id;
+	}
+	public class PlanBranch
+	{
+		public QualityReq[] qualityRequirements;
+		public int id;
+		public string planKey;
+		public string name;
+		public string description;
+		public int currencyCost;
+		public int actionCost;
+		public string buttonText;
+		public string image;
+		public bool isLocked;
+		public int ordering;
+		public bool actionLocked;
+		public bool currencyLocked;
+		public bool qualityLocked;
+		public Challenge[] challenges;
+	}
+	public class PlanQuery
+	{
+		public string name;
+		public string planKey;
+		public int? id;
+	}
+	public class PlanResult
+	{
+		public Plan plan;
+		public string message;
+		public bool isSuccess;
+	}
+	public class Plans
+	{
+		public Plan[] active;
+		public Plan[] complete;
+	}
+	public class Possession
+	{
+		public long qualityPossessedId;
+		public string name;
+		public string nameAndLevel;
+		public string description;
+		public string nature;
+		public string category;
+		public int effectiveLevel;
+		public int level;
+		public int himbleLevel;
+		public string availableAt;
+		public bool equippable;
+		public string bonusOrPenaltyDisplay;
+		public int progressAsPercentage;
+		public string image;
+		public long id;
+		public long? useEventId;
+	}
+	public class PossessionCategory
+	{
+		public string[] categories;
+		public string name;
+		public Possession[] possessions;
+		public string appearance;
+	}
+	public class QualityReq
+	{
+		public long qualityId;
+		public long id;
+		public string qualityName;
+		public string tooltip;
+		public string category;
+		public string nature;
+		public string status;
+		public bool isCost;
+		public string image;
+		public string availableAtMessage;
+	}
+	public class Shop
+	{
+		public string name;
+		public string image;
+		public string description;
+		public int ordering;
+		public int id;
+	}
+	public class ShopAvailability
+	{
+		public ShopItemQuality quality;
+		public int cost;
+		public int sellPrice;
+		public long id;
+	}
+	public class ShopItem
+	{
+		public ShopAvailability availability;
+		public bool forSale;
+	}
+	public class ShopItemQuality
+	{
+		public long id;
+		public string name;
+		public string description;
+		public long useEventId;
+		public string category;
+		public string image;
+	}
+	public class SocialAct
+	{
+		public InviteeData inviteeData;
+		public Branch branch;
+		public bool uniqueActPending;
+		public string actMessagePreview;
+		public string urgency;
+		public bool isSocialEvent;
+	}
 	public class StandardMessage
 	{
 		public Possession possession;
@@ -407,115 +391,80 @@ namespace fl
 		public string image;
 		public string tooltip;
 	}
-	public class Messages
-	{
-
-		public BasicMessage[] difficultyMessages;
-		public BasicMessage[] defaultMessages;
-		public StandardMessage[] standardMessages;
-	}
-
-	public class Event
+	public class Storylet
 	{
 		public string name;
-		public string description;
-		public bool isInEventUseTree;
-		public string frequency;
-		public string image;
-		public long id;
-	}
-
-	public class Exchange
-	{
-		string name;
-		string title;
-		string description;
-		Shop[] shops;
-		string image;
-
-		int id;
-	}
-	public class Shop
-	{
-		string name;
-		string image;
-		string description;
-		int ordering;
-		int id;
-	}
-
-	public class ExchangeResponse
-	{
-		Exchange exchange;
-		bool isSuccess;
-	}
-	public class InviteeData
-	{
-		public long branchId;
-		public string actQReqText;
-		public string actInviterQReqText;
-		public int addedFriendId;
-		public Friend[] eligibleFriends;
-	}
-	public class Friend
-	{
-		public long userId;
-		public long id;
-		public string name;
-		public string userName;
-	}
-
-	public class Interaction
-	{
-		public string type;
-		public string image;
-		public long relatedId;
-		public string description;
-		public string date;
-		public string ago;
-	}
-	public class SocialAct
-	{
-		public InviteeData inviteeData;
-		public Branch branch;
-		public bool uniqueActPending;
-		public string actMessagePreview;
-		public string urgency;
-		public bool isSocialEvent;
-	}
-
-	public class EndStorylet
-	{
-		public int rootEventId;
-		public int currentActionsRemaining;
-
-		[JsonProperty(PropertyName = "event")]
-		public Event eventValue;
-		public bool isLinkingEvent;
-		public bool isDirectLinkingEvent;
-		public int maxActionsAllowed;
-		public bool premiumBenefitsApply;
-		public string image;
-		public bool canGoAgain;
-	}
-
-	public class Possession
-	{
-		public long qualityPossessedId;
-		public string name;
-		public string nameAndLevel;
-		public string description;
-		public string nature;
+		public string teaser;
 		public string category;
-		public int effectiveLevel;
-		public int level;
-		public int himbleLevel;
-		public string availableAt;
-		public bool equippable;
-		public string bonusOrPenaltyDisplay;
-		public int progressAsPercentage;
+		public QualityReq[] qualityRequirements;
 		public string image;
 		public long id;
-		public long? useEventId;
+		public bool? isLocked;
+		public string frequency;
+		public string distribution;
+		public string description;
+		public string urgency;
+		public bool isInEventUseTree;
+		public bool? canGoBack;
+		public Branch[] childBranches;
 	}
+	public class StoryletList
+	{
+		public int actions;
+		public string phase;
+		public Storylet[] storylets;
+		public Storylet storylet;
+		public EndStorylet endStorylet;
+		public SocialAct socialAct;
+		public Messages messages;
+		public bool isSuccess;
+	}
+	public class SuccessMessage
+	{
+		public bool? isSuccess;
+		public string message;
+	}
+	public class SuccessResult
+	{
+		public bool isSuccess;
+	}
+	public class TransactionResult
+	{
+		public Possession[] possessionsChanged;
+		public string message;
+		public bool isSuccess;
+	}
+	public class User
+	{
+		public UserArea area;
+		public UserSetting setting;
+		public bool hasCharacter;
+		public string privilegeLevel;
+		public UserData user;
+	}
+	public class UserArea
+	{
+		public int id;
+		public string name;
+		public string description;
+		public string image;
+		public bool hideName;
+		public bool showOps;
+		public bool premiumSubRequired;
+	}
+	public class UserData
+	{
+		public string name;
+		public string emailAddress;
+		public int nex;
+		public int id;
+	}
+	public class UserSetting
+	{
+		public string name;
+		public bool canTravel;
+		public int id;
+		public bool itemsUsableHere;
+	}
+
 }
